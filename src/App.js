af4f6af3445a2510baac382dcd8ad671ccbf9485
Arjun Sedani <sedaniarjun@gmail.com>
2019-12-10 12:39:45 +0530

    drgable

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React from 'react';
+import React from "react";
 import scenegltf from "./scene.glb";
 
 function App() {
-
   const handlePopoverOpen = event => {
-   console.log('called this');
+    console.log("called this");
   };
-
-  
-
 
   return (
     <div className="App">
-      <div className="cybertruck2"/>
-        <h2>Use Effect</h2>
-    <a-scene antialias="true" background="color: #555555">
-      <a-entity light="type: ambient; intensity:2.8; color: #C8C8FF"></a-entity>
-       <a-entity light="type: spot; intensity:.3; " position="1.1 2.7 1"></a-entity>
-<a-entity line="start: 0, 1, 0; end: 2 0 -5; color: red"
-          line__2="start: -2, 4, 5; end: 0 4 -3; color: green"></a-entity>
-       
-  <a-entity gltf-model={scenegltf} scale="0.1 0.1 0.1" position="0 1 -1" rotation="0 0 0" rotate-on-click="_animate: true;" >
-    
-       <a-animation 
-                    begin="rotateModel"
-                    attribute="rotation"
-                    dur="1000"
-                    direction="alternate"
-                    to="0 180 0"
-               ></a-animation>
-    </a-entity>
-    <a-plane id="rotateBtn" class="clickable" color="#CCC" height=".25" width=".75" position="1 1.5 -1.2">
-      <a-entity text="align: center; anchor: center; value: ROTATE; color: black; wrap-count: 10;" onmouseover={handlePopoverOpen}></a-entity>
-    </a-plane>
-    <a-entity  id="controller" laser-controls custom-controls raycaster="objects: .clickable"></a-entity>
-      <a-camera>
-        <a-cursor raycaster="objects: .clickable"></a-cursor>
-      </a-camera>
-
-</a-scene>
-
+      <a-scene background="color: #555555">
+        <a-entity light="type: ambient; intensity:2.8; color: #C8C8FF"></a-entity>
+        <a-entity
+          light="type: spot; intensity:.3; "
+          position="1.1 2.7 1"
+        ></a-entity>
+        <a-entity
+          gltf-model={scenegltf}
+          position="0 -2.930 -29"
+          click-drag=""
+          wasd-controls="acceleration: 400; wsEnabled: true"
+          // scale="0.1 0.1 0.1"
+          rotation="0 0 0"
+          rotate-on-click="_animate: true;"
+        ></a-entity>
+           {/* <a-camera look-controls-enabled="false"></a-camera> */}
+        <a-camera>
+          <a-cursor
+            position="0 0 -1"
+            geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+            material="color: cyan; shader: flat"
+            raycaster="objects: .clickable"
+          ></a-cursor>
+        </a-camera>
+      </a-scene>
     </div>
   );
 }
