da81a8558eedf8035ba4525a8000aa22f03cccb0
Sedani Arjun Bhavesh (RBEI/EHC1) <esd3kor@bosch.com>
2019-12-11 22:08:19 +0530

    event triggers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import scenegltf from "./scene.glb";
 
+const AFRAME = window.AFRAME;
+
+
 function App() {
   const handlePopoverOpen = event => {
     console.log("called this");
   };
-
+  
   return (
     <div className="App">
       <a-scene background="color: #555555">
@@ -22,15 +25,26 @@ function App() {
           // scale="0.1 0.1 0.1"
           rotation="0 0 0"
           rotate-on-click="_animate: true;"
+          // event-set__enter="_event: mouseenter; material.color: yellowgreen; scale: 3 1 1"
+          event-set__enter={`_event: mouseenter; _target: #cylinderText; rotation:40 40 0`}
+          event-set__leave="_event: mouseleave; material.color: skyblue; scale: 1 1 1">
         ></a-entity>
+        <a-box id="cylinderText" position="0 3.5 -2" rotation="30 30 0" color="skyblue" event-set__enter="_event: mouseenter; material.color: yellowgreen; scale: 3 1 1" event-set__leave="_event: mouseleave; material.color: skyblue; scale: 1 1 1" scale="" visible="" material="" geometry="">
+            <a-animation attribute="rotation" begin="click" dur="500" fill="backwards" to="30 30 360"></a-animation>
+         </a-box>
            {/* <a-camera look-controls-enabled="false"></a-camera> */}
         <a-camera>
-          <a-cursor
+          {/* <a-cursor
             position="0 0 -1"
             geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
             material="color: cyan; shader: flat"
             raycaster="objects: .clickable"
-          ></a-cursor>
+          ></a-cursor> */}
+           <a-entity cursor="fuse: true; maxDistance: 30; timeout: 500"
+            position="0 0 -1"
+            geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+            material="color: white; shader: flat">
+          </a-entity>
         </a-camera>
       </a-scene>
     </div>
